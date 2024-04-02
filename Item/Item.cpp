b8d97bea9accf3ff804583d8d6b8a1585787c8e6
Benardo07 <117990908+Benardo07@users.noreply.github.com>
2024-04-03 04:06:25 +0700

    feat : add Building

--- Item/Item.cpp
+++ Item/Item.cpp
@@ -2,6 +2,7 @@
 #include "PlantConfig.hpp"
 #include "AnimalConfig.hpp"
 #include "ProductConfig.hpp"
+#include "RecipeConfig.hpp"
 
 // class Item
 int countIdItem = 1;
@@ -206,4 +207,28 @@ string Product::getOrigin() const{
 
 float Product::getAddedWeight() const{
     return this->addedWeight;
+}
+
+//class Building
+Building::Building():Item(){
+
+}
+
+Building::Building(string name):Item(RecipeConfig::recipes[name].name,RecipeConfig::recipes[name].code,RecipeConfig::recipes[name].price){
+    this->material = RecipeConfig::recipes[name].materials;
+}
+
+int Building::getQuantityPerMaterial(string materialName) const{
+    try {
+        return material.at(materialName);
+    } catch (const std::out_of_range& e) {
+        
+        
+        return 0;
+    }
+}
+
+
+map<string, int> Building::getBuildingMaterial() const{
+    return this->material;
 }
--- Item/Item.hpp
+++ Item/Item.hpp
@@ -9,93 +9,103 @@
 #include "configClass/ProductConfig.hpp"
 using namespace std;
 
+class Item
+{
+protected:
+    static int countIdItem;
+    int itemId;
+    string name;
+    string code;
+    int price;
 
-class Item{
-    protected :
-        static int countIdItem;
-        int itemId;
-        string name;
-        string code; 
-        int price;
-
-    public :
-        Item();
-        Item(string name, string code, int price);
-        void setItemId(int itemId);
-        void setName(string name);
-        void setCode(string code);
-        void setPrice (int price);
-        int getItemId() const;
-        string getName() const;
-        string getCode() const;
-        int getPrice() const;
+public:
+    Item();
+    Item(string name, string code, int price);
+    void setItemId(int itemId);
+    void setName(string name);
+    void setCode(string code);
+    void setPrice(int price);
+    int getItemId() const;
+    string getName() const;
+    string getCode() const;
+    int getPrice() const;
 };
 
-class Plant : public Item{
-    private :
-        PlantType type;
-        int plantId;
-        int durationToHarvest;
-        int age;
-    
-    public :
-        Plant();
-        Plant(string name, int age);
-        void setPlantType(PlantType type);
-        void setPlantId(int plantId);
-        void setDurationToHarvest(int durationToHarvest);
-        void setAge(int age);
-        PlantType getPlantType() const;
-        int getPlantId() const;
-        int getDurationToHarvest() const;
-        int getAge() const;
-        Plant& operator++();
-        Plant operator++(int );
+class Plant : public Item
+{
+private:
+    PlantType type;
+    int plantId;
+    int durationToHarvest;
+    int age;
+
+public:
+    Plant();
+    Plant(string name, int age);
+    void setPlantType(PlantType type);
+    void setPlantId(int plantId);
+    void setDurationToHarvest(int durationToHarvest);
+    void setAge(int age);
+    PlantType getPlantType() const;
+    int getPlantId() const;
+    int getDurationToHarvest() const;
+    int getAge() const;
+    Plant &operator++();
+    Plant operator++(int);
 };
 
-class Animal : public Item{
-    private :
-        AnimalType type;
-        int animalId;
-        float weight;
-        float weighToHarvest;
+class Animal : public Item
+{
+private:
+    AnimalType type;
+    int animalId;
+    float weight;
+    float weighToHarvest;
 
-    public :
-        Animal();
-        Animal(string name, float weight);
-        void setAnimalType(AnimalType type);
-        void setAnimalId(int animalId);
-        void setWeight(float weight);
-        void setWeightToHarvest(float weightToHarvest);
-        AnimalType getAnimalType() const;
-        int getAnimalId() const;
-        float getWeight() const;
-        float getWeightToHarvest() const;
-        Animal& operator+(Product product);
+public:
+    Animal();
+    Animal(string name, float weight);
+    void setAnimalType(AnimalType type);
+    void setAnimalId(int animalId);
+    void setWeight(float weight);
+    void setWeightToHarvest(float weightToHarvest);
+    AnimalType getAnimalType() const;
+    int getAnimalId() const;
+    float getWeight() const;
+    float getWeightToHarvest() const;
+    Animal &operator+(Product product);
 };
 
-class Product : public Item{
-    private :
-        ProductType type;
-        int productId;
-        string origin;
-        float addedWeight;
+class Product : public Item
+{
+private:
+    ProductType type;
+    int productId;
+    string origin;
+    float addedWeight;
 
-    public :
-        Product();
-        Product(string name);
-        void setProductType(ProductType type);
-        void setProductId(int productId);
-        void setOrigin(float origin);
-        void setAddedWeight(float addedWeight);
-        ProductType getProductType() const;
-        int getProductId() const;
-        string getOrigin() const;
-        float getAddedWeight() const;
+public:
+    Product();
+    Product(string name);
+    void setProductType(ProductType type);
+    void setProductId(int productId);
+    void setOrigin(float origin);
+    void setAddedWeight(float addedWeight);
+    ProductType getProductType() const;
+    int getProductId() const;
+    string getOrigin() const;
+    float getAddedWeight() const;
 };
 
-class Building : public Item{
-    private :
+class Building : public Item
+{
+private:
+    map<string, int> material;
+public:
+    Building();
+    Building(string name);
+    int getQuantityPerMaterial(string materialName) const;
+    map<string, int> getBuildingMaterial() const;
 };
 
 #endif
